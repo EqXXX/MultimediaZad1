caf2445a0911eb0d7133ea4d7adc70dc2a1d740c
EqXXX <Piotrowicz98Kamil@gmail.com>
2023-03-30 19:16:42 +0200

    Gaem

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    float HorizontalMove = 0f;
+
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(HorizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+    public void OnLanding ()
+    {
+            animator.SetBool("IsJumping", false);
+    }
+    public void OnCrouching (bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+    
+
+    void FixedUpdate ()
+    {
+        //Poruszanie siê
+        controller.Move(HorizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
